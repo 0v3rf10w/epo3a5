a162a78cd50501ae1da03415d91a207b947ec8b0
Marc Zwalua <M.Zwalua@student.tudelft.nl>
2014-11-24 10:02:17 +0100

    Update pwm.vhd

--- chip/subsystems/PWM/pwm.vhd
+++ chip/subsystems/PWM/pwm.vhd
@@ -1,1 +1,11 @@
-test
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity pwm is
+port(
+	clk, reset : in std_logic;
+	pwm : out std_logic;
+	control : in std_logic_vector(9 downto 0);
+	zero, eq : out std_logic
+);
+end pwm;
