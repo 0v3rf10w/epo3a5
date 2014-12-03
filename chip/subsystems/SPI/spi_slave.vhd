2b9e9c0487a10b66fcb7c0495d4758af4dc3cbbc
Job Tijhuis <jobstijhuis@gmail.com>
2014-12-03 16:11:36 +0100

    removed shift_in latch because this is now handled in the shift register

--- chip/subsystems/SPI/spi_slave.vhd
+++ chip/subsystems/SPI/spi_slave.vhd
@@ -39,12 +39,12 @@ architecture behavioural of spi_slave is
 	signal count : std_logic_vector(3 downto 0);
 	signal shift_output, output_buffer : std_logic_vector(7 downto 0);
 	signal count_reset : std_logic;
-	signal shift,shift_in : std_logic;
+	signal shift : std_logic;
 	
 begin
 
 cnt1:  counter port map (sclk,count_reset,count);
-shft1: shift_reg port map (sclk,reset,shift,shift_in,'0',"11111111",shift_output);
+shft1: shift_reg port map (sclk,reset,shift,mosi,'0',"11111111",shift_output);
 	
 	read_out <= output_buffer;
 	miso <= '1'; -- we are not sending anything from the slave
@@ -85,6 +85,4 @@ shft1: shift_reg port map (sclk,reset,shift,shift_in,'0',"11111111",shift_output
 		end if;
 	end process;
 
-	shift_in <= mosi when sclk = '0' else
-			shift_in;
 end behavioural;
