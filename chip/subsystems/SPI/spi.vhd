7e633a893cf34b02687e6fc610f4781363814cc4
Job Tijhuis <jobstijhuis@gmail.com>
2014-12-03 16:51:04 +0100

    shift_reg now works on inverted clock so it is in sync with the slave clock

--- chip/subsystems/SPI/spi.vhd
+++ chip/subsystems/SPI/spi.vhd
@@ -44,7 +44,7 @@ port(	clk:			in std_logic;
 		shift:			out std_logic;
 		sclk:			out std_logic;
 		c_reset:		out std_logic;
-		ss:				out std_logic;
+		ss:			out std_logic;
 		busy:			out std_logic
 	);
 end component control;
@@ -54,13 +54,15 @@ signal count : std_logic_vector(3 downto 0);
 signal output : std_logic_vector(7 downto 0);
 signal count_reset,slave_clk : std_logic;
 signal shift_reset,shift : std_logic;
+signal inv_clk : std_logic;
 
 begin
 	sclk <= slave_clk;
 	read_out <= output;
 	mosi <= output(7);
+	inv_clk <= not clk;
 cnt1:  counter port map (slave_clk,count_reset,count);
-shft1: shift_reg port map (slave_clk,reset,shift,miso,write_enable,write_in,output);
+shft1: shift_reg port map (inv_clk,reset,shift,miso,write_enable,write_in,output);
 ctrl1: control port map (clk,reset,send,count,shift,slave_clk,count_reset,ss,busy);
 
 end structural;
