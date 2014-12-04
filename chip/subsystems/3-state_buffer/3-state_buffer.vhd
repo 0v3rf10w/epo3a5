96b91f4f387cd346dbfc9a390a171cc6176270be
baukemeekes <baukemeekes@hotmail.com>
2014-12-04 16:37:20 +0100

    Unieke registers en buffers met eigen adres

--- chip/subsystems/3-state_buffer/3-state_buffer.vhd
+++ chip/subsystems/3-state_buffer/3-state_buffer.vhd
@@ -1,17 +1,250 @@
 package IEEE;
 use IEEE.std_logic_1164.all;
 
-entity buffer is
+----------------------------------------------------------------------------------------------------
+--Buffer A (BA)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_A is
 port( buf_in  : in  std_logic_vector(7 downto 0);
       buf_oe  : in  std_logic;
       buf_out : out std_logic_vector(7 downto 0));
 end entity buffer;
 
-architecture behaviour of buffer is
+architecture behaviour of buf_A is
 begin
   process (buf_in, buf_oe)
   begin
     if (buf_oe = '1') then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer In (Bin)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_in is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic;
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_in is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe = '1') then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 1 (B1)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_1 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_1 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=1)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 2 (B2)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_2 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_2 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=2)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 3 (B3)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_3 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_3 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=3)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 4 (B4)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_4 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_4 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=4)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 5 (B5)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_5 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_5 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=5)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+ 
+  ----------------------------------------------------------------------------------------------------
+--Buffer 6 (B6)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_6 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_6 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=6)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 7 (B7)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_7 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_7 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=7)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 8 (B8)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_8 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_8 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=8)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 9 (B9)-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_9 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_9 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=9)) then
+      buf_out <= buf_in;
+    else
+      buf_out <= "ZZZZZZZZ"
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Buffer 10 (B10)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity buf_10 is
+port( buf_in  : in  std_logic_vector(7 downto 0);
+      buf_oe  : in  std_logic_vector(4 downto 0);
+      buf_out : out std_logic_vector(7 downto 0));
+end entity buffer;
+
+architecture behaviour of buf_10 is
+begin
+  process (buf_in, buf_oe)
+  begin
+    if (buf_oe(4) = '1' AND (unsigned(buf_oe(3 downto 0))=10)) then
       buf_out <= buf_in;
     else
       buf_out <= "ZZZZZZZZ"
--- chip/subsystems/Register/register.vhd
+++ chip/subsystems/Register/register.vhd
@@ -1,7 +1,11 @@
 package IEEE;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
-entity register is
+----------------------------------------------------------------------------------------------------
+--Register A (RA)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_A is
 port (  reg_in  : in  std_logic_vector(7 downto 0);
         reg_clk : in  std_logic;
         reg_rst : in  std_logic;
@@ -9,13 +13,243 @@ port (  reg_in  : in  std_logic_vector(7 downto 0);
         reg_out : out std_logic_vector(7 downto 0));
 end entity register;
 
-architecture behaviour of register is
+architecture behaviour of reg_A is
 begin
   process(reg_clk, reg_rst)
   begin
     if (reg_rst = '1') then
       reg_out <= "00000000";
     elsif (reg_clk'event AND reg_clk='1' AND reg_ld='1') then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 1 (R1)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_1 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_1 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=1) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 2 (R2)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_2 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_2 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=2) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 3 (R3)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_3 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_3 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=3) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 4 (R4)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_4 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_4 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=4) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 5 (R5)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_5 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_5 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=5) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 6 (R6)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_6 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_6 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=6) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 7 (R7)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_7 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_7 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=7) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 8 (R8)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_8 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_8 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=8) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 9 (R9)-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_9 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_9 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=9) then
+      reg_out <= reg_in;
+    end if;
+  end process;
+end architecture;
+
+----------------------------------------------------------------------------------------------------
+--Register 10 (R10)---------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+entity reg_10 is
+port (  reg_in  : in  std_logic_vector(7 downto 0);
+        reg_clk : in  std_logic;
+        reg_rst : in  std_logic;
+        reg_ld  : in  std_logic_vector(4 downto 0);
+        reg_out : out std_logic_vector(7 downto 0));
+end entity register;
+
+architecture behaviour of reg_10 is
+begin
+  process(reg_clk, reg_rst)
+  begin
+    if (reg_rst = '1') then
+      reg_out <= "00000000";
+    elsif (reg_clk'event AND reg_clk='1' AND reg_ld(4)='1' AND (unsigned(reg_ld(3 downto 0)=10) then
       reg_out <= reg_in;
     end if;
   end process;
