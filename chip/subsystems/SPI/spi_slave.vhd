f7317d2f97cd2b8c0f18d192e95e7bc843c1b2ec
Job Tijhuis <jobstijhuis@gmail.com>
2014-12-06 16:41:43 +0100

    data buffer removed data_ready bit now indicates when data is ready

--- chip/subsystems/SPI/spi_slave.vhd
+++ chip/subsystems/SPI/spi_slave.vhd
@@ -4,6 +4,7 @@ use IEEE.std_logic_1164.all;
 entity spi_slave is
 	port (	reset		: in	std_logic;
 		read_out	: out	std_logic_vector (7 downto 0);
+		data_ready	: out	std_logic;
 		
 		sclk		: in	std_logic;
 		mosi		: in	std_logic;
@@ -45,8 +46,11 @@ begin
 cnt1:  counter port map (sclk,count_reset,count);
 shft1: shift_reg port map (sclk,reset,shift,shift_in,'0',"11111111",shift_output);
 	
-	read_out <= output_buffer;
+	read_out <= shift_output;
 	miso <= '1'; -- we are not sending anything from the slave
+	shift <= '1'; -- always shift because sclk determines when to shift
+	
+	data_ready <= '1' when (count = "0000") else '0';
 	
 	process(sclk,reset)
 	begin
@@ -62,30 +66,16 @@ shft1: shift_reg port map (sclk,reset,shift,shift_in,'0',"11111111",shift_output
 	process(sclk,reset)
 	begin
 		if(reset = '1') then
-			output_buffer <= (others => '0');
-		else
-			if(falling_edge(sclk)) then
-				output_buffer <= new_output_buffer;
-			end if;
-		end if;
-	end process;
-
-	process(count,reset)
-	begin
-		if(reset = '1') then
-			shift <= '0';
 			count_reset <= '1';
-			new_output_buffer <= (others => '0');
 		else
-			shift <= '1';
 			if(count = "1000") then
-				new_output_buffer <= shift_output;
 				count_reset <= '1';
 			else
-				new_output_buffer <= output_buffer;
 				count_reset <= '0';
 			end if;
 		end if;
 	end process;
+
+
 
 end behavioural;
