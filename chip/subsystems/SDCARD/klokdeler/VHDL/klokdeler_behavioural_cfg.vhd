9b3691696d2f30a21a6f54891c56d81f8f69eb24
Job Tijhuis <jobstijhuis@gmail.com>
2014-12-11 11:06:26 +0100

    haat aan niels

--- chip/subsystems/SDCARD/klokdeler/VHDL/klokdeler_behavioural_cfg.vhd
+++
@@ -1,8 +0,0 @@
-configuration klokdeler_behavioural_cfg of klokdeler is
-   for behavioural
-      for all: dff use configuration work.dff_behavioural_cfg;
-      end for;
-   end for;
-end klokdeler_behavioural_cfg;
-
-
--- chip/subsystems/SDCARD/klokdeler/VHDL/tb_behaviouralsynt_cfg.vhd
+++
@@ -1,8 +0,0 @@
-configuration tb_behaviouralsynt_cfg of tb is
-   for behavioural
-      for all: klokdeler use configuration work.klokdeler_synthesised_cfg;
-      end for;
-   end for;
-end tb_behaviouralsynt_cfg;
-
-
