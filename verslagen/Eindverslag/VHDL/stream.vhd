21a022c26f2412329b0daf8f361aed260c9c2617
jordyvanderhor <j.vanderhorst@student.tudelft.nl>
2015-01-12 23:06:46 +0100

    blackbox vhdl toegevoegd

---
+++ verslagen/Eindverslag/VHDL/stream.vhd
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity stream is
+   port(IN_0 :in    std_logic;
+        IN_1 :in    std_logic;
+        OUT_0:out   std_logic;
+        OUT_1:out   std_logic;
+        E    :in    std_logic);
+=======
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity stream is
+   port(IN_0 :in    std_logic;
+        IN_1 :in    std_logic;
+        OUT_0:out   std_logic;
+        OUT_1:out   std_logic;
+        E    :in    std_logic);
+>>>>>>> origin/master
+end stream;
+
+
