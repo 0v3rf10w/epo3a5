c018012b2625f72581dc47a8c2c3b75aa80c69c3
Job Tijhuis <jobstijhuis@gmail.com>
2015-01-19 12:25:14 +0100

    samengevoegd

--- chip/subsystems/SPI_slave/shift_reg.vhd
+++ chip/subsystems/SPI_slave/shift_reg.vhd
@@ -9,6 +9,38 @@ port(	clk: 		in std_logic;
 	);
 end shift_reg;
 
+architecture behaviour of shift_reg is
+
+signal reg_shift,shifted_reg: std_logic_vector (7 downto 0);
+
+begin
+
+	shifted_reg(7 downto 1) <= reg_shift(6 downto 0);
+	shifted_reg(0) <= shift_in;
+	
+	process(clk,reset)
+	begin
+	if(reset = '1') then
+		reg_shift <= (others => '0');
+	else
+		if(falling_edge(clk)) then
+			reg_shift <= shifted_reg;						
+		end if;
+	end if;
+	end process;
+
+	output <= reg_shift;
+end behaviour;
+
+
+
+
+
+
+
+
+
+
 
 
 
